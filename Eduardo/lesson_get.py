cd1e1db899637ce67e6cfb01827a52268299930d
Vitor Carmo Vannuchi <vitor.vannuchi@gmail.com>
2021-05-03 19:41:38 -0300

    Pastas do dudu
    Codigos recentes do eduardo

---
+++ Eduardo/lesson_get.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = "https://futdb.app/api/clubs"
+
+_headers = {"X-AUTH-TOKEN":"ca379bac-5fcd-4e13-b08c-03982189b0fb"}
+_params = { "page":"1",
+			"limit":"20"}
+
+x = requests.get(url, headers=_headers, params=_params)
+
+jsonResposta = x.json()
