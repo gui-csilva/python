5701d28cdf80b03c72e309857e2868e66b5fdc0e
Vitor Carmo Vannuchi <vitor.vannuchi@gmail.com>
2021-05-24 19:45:51 -0300

    Criando classe cargo
    criando a classe cargo.

---
+++ Python/cargo.py
@@ -0,0 +1,22 @@
+class cargo:
+	def __init__(self, nome, mediaSalarial, horasTrabalhadas):
+		self.nome = nome
+		self.mediaSalarial = mediaSalarial
+		self.horasTrabalhadas = horasTrabalhadas
+
+	def getNome(self):
+		return self.nome
+
+	def getMediaSalarial(self):
+		return self.mediaSalarial
+	
+	def getHorasTrabalhadas(self):
+		return self.horasTrabalhadas
+
+	def aumentarPercentualSalario(self, percentual):
+		self.mediasalarial = mediasalarial + (mediaSalarial * (percentual/100) )
+		return self.getMediaSalarial()
+
+	def setHorasTrabalhadas(self, horasNovas):
+		self.horasTrabalhadas = horasNovas
+		return self.horasTrabalhadas
