a099a29b584f9af84f76f04e46a8a8c1cb4c69d3
Guilherme Costa e Silva <guilherme.costas0910@gmail.com>
2021-06-08 19:51:15 -0300

    coisas feitas na aula

--- Python/Empresa/Rh.py
+++ Python/Empresa/Rh.py
@@ -2,6 +2,7 @@ import Pessoa
 class Rh:
 	def __init__(self, pessoa):
 		self.pessoa = pessoa
+		self.ferias = False
 
 	def salarioComPlr(self):
 		salario = self.pessoa.getSalario()
@@ -11,4 +12,19 @@ class Rh:
 
 	def plr(self):
 		bonus = self.pessoa.getSalario() * 2.5
-		return bonus		
+		return bonus
+
+	def setFerias(self, ferias):
+		self.ferias = ferias
+
+	def getFerias(self):
+		return self.ferias
+
+	def valorFerias(self):
+		tercoSalario = self.pessoa.getSalario() / 3
+		return tercoSalario
+
+	def retornarSalarioComTerco(self):
+		tercoSalario = self.pessoa.getSalario() / 3
+		resultadoTerco = self.pessoa.getSalario() + tercoSalario
+		return resultadoTerco
--- Python/Empresa/principal.py
+++ Python/Empresa/principal.py
@@ -1,14 +1,34 @@
 import Cargo
 import Pessoa
 import Rh
+import random
 
 estagiario = Cargo.Cargo("estagiario", 1300.00, 20)
 secretaria = Cargo.Cargo("secretaria", 2200.00, 40)
 supervisor = Cargo.Cargo("supervisor", 6000.00, 60)
 
-pessoaA = Pessoa.Pessoa("Junior", "M", 13, 64889900, 43237168351, 302163271, 886535673, 4000.50, estagiario)
-pessoaB = Pessoa.Pessoa("Julia", "F", 22, 79172334, 45910735481, 111225368, 547351564, 1000.90, secretaria)
-pessoaC = Pessoa.Pessoa("Sebastião", "M", 45, 73921539, 3710018624130, 142623567, 789017203, 2500.75, supervisor)
+funcionarios = [
+	Pessoa.Pessoa("Junior", "M", 13, 64889900, 43237168351, 302163271, 886535673, 4000.50, estagiario),
+	Pessoa.Pessoa("Julia", "F", 22, 79172334, 45910735481, 111225368, 547351564, 1000.90, secretaria),
+	Pessoa.Pessoa("Sebastião", "M", 45, 73921539, 3710018624130, 142623567, 789017203, 2500.75, supervisor)
+]
+
+# print(type(funcionarios[0]))
+
+for value in funcionarios:
+	pessoaRh = Rh.Rh(value)
+	print(value.getNome())
+	print("Salario: ", value.getSalario())
+	print("plr: ", pessoaRh.plr())
+	print("salarioComPlr: ", pessoaRh.salarioComPlr())
+	if random.randrange(1,3) == 2:
+		pessoaRh.setFerias(True)
+	if pessoaRh.getFerias() == True:
+		print("férias: ", pessoaRh.valorFerias())
+		print("salarioComFérias: ", pessoaRh.retornarSalarioComTerco())
+	print()
+
+
 
 # pessoaA.printPessoa()
 # print()
@@ -17,8 +37,3 @@ pessoaC = Pessoa.Pessoa("Sebastião", "M", 45, 73921539, 3710018624130, 14262356
 # pessoaC.printPessoa()
 # print("----")
 # print(type(secretaria))
-
-pessoaRh = Rh.Rh(pessoaC)
-print("Salario: ", pessoaC.getSalario())
-print("plr: ", pessoaRh.plr())
-print("salarioComPlr: ", pessoaRh.salarioComPlr())
